2a5c54308c367e8de3b4d701f9c44c26ac75eee6
doitforjay <jayda94@gmail.com>
2016-10-16 19:39:50 -0400

    testing out spark

---
+++ backend/src/App.java
@@ -0,0 +1,14 @@
+package com.myproject.test_app;
+import static spark.Spark.*;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	 get("/hello", (req, res) -> "Hello World");
+    }
+}
