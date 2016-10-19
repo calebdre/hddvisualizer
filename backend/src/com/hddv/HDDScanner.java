ee9ccec7ca10451a0534f5b6a4d987ba4f6e6f28
HarshaGoli <harshagoli@gmail.com>
2016-10-18 22:53:44 -0400

    HDDScanner now scans the hdd from '~/'; there are bugs

---
+++ backend/src/com/hddv/HDDScanner.java
@@ -0,0 +1,24 @@
+package com.hddv;
+
+import java.io.File;
+//import static spark.Spark.get;
+
+public class HDDScanner
+{
+
+    public static void main( String[] args )
+    {
+        System.out.println("Starting Scan");
+        list(new File("/Users/HarshaGoli/"));
+    }
+
+    public static void list(File file) {
+        System.out.println(file.getName());
+        File[] children = file.listFiles();
+        for (File child : children) {
+            //TODO: Add if statement to stop recursive if the child is not a folder
+            list(child);
+        }
+    }
+
+}
