c4743c753f1d378e008278c20efadff7e3bda61f
Jajuan Brooks <jayda94@gmail.com>
2016-11-03 22:33:31 -0400

    Update Parser.java

--- backend/src/com/hddv/Parser.java
+++ backend/src/com/hddv/Parser.java
@@ -4,7 +4,6 @@ import com.google.gson.reflect.TypeToken;
 
 
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.lang.reflect.Type;
 import java.nio.file.Files;
@@ -14,68 +13,55 @@ import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 import org.apache.commons.io.FilenameUtils;
 public class Parser {
 	public static void main(String [] args) throws IOException{
 
-		//Searches through directory and puts all contents in list "files"
-		File f = new File("/Users/new/Desktop/");
-		ArrayList<File> files = new ArrayList<File>(Arrays.asList(f.listFiles()));
+		System.out.println("enter full directory: ");
+        Scanner in = new Scanner(System.in);
+        String input = in.nextLine();
+        System.out.println(parse(input));
+        in.close();
+		
+}
 
+	static String parse(String filePath) throws IOException{
+		
+		//Searches through directory and puts all contents in list "files"
+		File f = new File(filePath);
+		ArrayList<File> files = new ArrayList<File>(Arrays.asList(f.listFiles()));
+		
+		//gets each file name in directory
+		File f1 = new File(filePath);
+		ArrayList<String> names = new ArrayList<String>(Arrays.asList(f1.list()));
+		
+		List<HDDVFile> list = new ArrayList<HDDVFile>();
 		//grabs extension of file
-		String ext1 = FilenameUtils.getExtension(files.get(3).toString());
 		
-		//grabs full path of file
-		Path file = Paths.get(files.get(3).toString());
-		
-		//class that gets basic file attributes
-		BasicFileAttributes attr = Files.readAttributes(file, BasicFileAttributes.class);
-		
-		//print out info of file
-		System.out.println("path: " + file);
-		System.out.println("creationTime: " + attr.creationTime());
-		System.out.println("lastAccessTime: " + attr.lastAccessTime());
-		System.out.println("lastModifiedTime: " + attr.lastModifiedTime());
-		System.out.println("size: " + attr.size());
-		System.out.println("extension: " + ext1);
-	
-	//converting HDDVFile object properties to json
-        List<HDDVFile> list = new ArrayList<HDDVFile>();
-        for (int i = 0; i < 20; i++) {
-                list.add(new HDDVFile(files.get(3).toString(), attr.size(), attr.lastModifiedTime().toString(),attr.creationTime().toString(),files.get(3).toString(), ext1));
-        }
+
+		for (int i = 0; i < files.size(); i++) {
+			
+			//grabs extension of files
+			String ext1 = FilenameUtils.getExtension(files.get(i).toString());
+			
+			//grabs full path of file
+			Path file = Paths.get(files.get(i).toString());
+
+			//class that gets basic file attributes
+			BasicFileAttributes attr = Files.readAttributes(file, BasicFileAttributes.class);
+			
+            list.add(new HDDVFile(names.get(i), attr.size(), attr.lastModifiedTime().toString(),attr.creationTime().toString(),file.toString(), ext1));
+
+		}
+		//converting to json
         Gson gson = new Gson();
         Type type = new TypeToken<List<HDDVFile>>() {}.getType();
         String json = gson.toJson(list, type);
-        System.out.println(json);
-        List<HDDVFile> fromJson = gson.fromJson(json, type);
-//
-//        for (HDDVFile task : fromJson) {
-//                System.out.println(task);
-//        }
-}
-	
-
-	/*
-	 * ---------------This is for outputting to a json file ------------------------
-	 * 
-		creates the HDDFile object that contains all file info
-		HDDVFile currentFile = new HDDVFile("files.get(3).toString()", attr.size(), attr.lastModifiedTime().toString(),attr.creationTime().toString(),files.get(3).toString(), ext1);
-		 //converting to json
-		Gson gson = new Gson();
-	        String json = gson.toJson(currentFile);
-	        System.out.println(json);
-	        
-	        //creates json file
-	        try (FileWriter writer = new FileWriter("/Users/new/Desktop/out.json")) {
-
-	            gson.toJson(file, writer);
-
-	        } catch (IOException e) {
-	            e.printStackTrace();
-	        }
-	       */
+       
+		return json;
+	}
 	
 	
 	static long list(File file, long totalData) {
@@ -94,7 +80,6 @@ public class Parser {
 	
 	
 }
-   
 
 
     
